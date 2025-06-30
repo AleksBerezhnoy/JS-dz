19cf69301cbdafd83bb9a963528d2e42761351b9
Aleksandr Berezhnoy <berezhnoyfootball@gmail.com>
2025-06-30 22:52:58 +0300

    3-birthday success

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-function play(dice) {
-  const facets = parseInt(dice.slice(1), 10);
-  const randomFacets = Math.floor(Math.random() * facets + 1);
-  return randomFacets;
+function ageIs14(birthday) {
+  const nowDate = new Date();
+  const userBirthday = new Date(birthday);
+  if (nowDate.getFullYear() - userBirthday.getFullYear() > 14) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(play('d6'));
+console.log(ageIs14('2006-01-01'));
