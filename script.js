47eee17691edf6a4651a8485b401bfc825dccbee
Aleksandr Berezhnoy <berezhnoyfootball@gmail.com>
2025-07-01 21:36:07 +0300

    3-bithday-fixed

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 function ageIs14(birthday) {
-  const nowDate = new Date();
-  const userBirthday = new Date(birthday);
-  if (nowDate.getFullYear() - userBirthday.getFullYear() > 14) {
-    return true;
-  } else {
-    return false;
+  const now = new Date();
+  const birthDate = new Date(birthday);
+
+  let age = now.getFullYear() - birthDate.getFullYear();
+
+  const hasBirthdayPassed =
+    now.getMonth() > birthDate.getMonth() ||
+    (now.getMonth() === birthDate.getMonth() && now.getDate() >= birthDate.getDate());
+
+  if (!hasBirthdayPassed) {
+    age--;
   }
+
+  return age >= 14;
 }
 
-console.log(ageIs14('2006-01-01'));
+console.log(ageIs14('2010-07-02'));
