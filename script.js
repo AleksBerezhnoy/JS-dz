aad3a268629c7f75857eddfa7bb3d9ff260b957c
Aleksandr Berezhnoy <berezhnoyfootball@gmail.com>
2025-07-02 11:05:55 +0300

    4-oop fixed error

--- script.js
+++ script.js
@@ -3,26 +3,43 @@ const Character = function (species, name, language) {
   this.name = name;
   this.language = language;
 };
+Character.prototype.speak = function (){
+  console.log(this.name, this,this.language);
+}
+
 
 const Ork = function (species, name, language, weapon) {
-  Character.call(this, species, name, language);
+  Character.call(this, species, name, language); /
   this.weapon = weapon;
 };
 
-Ork.prototype.break = function (){
+
+Ork.prototype = Object.create(Character.prototype);
+Ork.prototype.constructor = Ork;
+
+
+Ork.prototype.break = function () {
   console.log(`Удар нанесен ${this.weapon}`);
-}
+};
+
 
 const Elf = function (species, name, language, incantations) {
-  Character.call(this, species, name, language);
+  Character.call(this, species, name, language); /
   this.incantations = incantations;
 };
-Elf.prototype.createIncantations = function (){
+
+
+Elf.prototype = Object.create(Character.prototype);
+Elf.prototype.constructor = Elf;
+
+
+Elf.prototype.createIncantations = function () {
   console.log(`Создалось заклинание под названием ${this.incantations}`);
-}
+};
 
 
-const ork = new Ork('orks', 'ork', 'ru', 'Мечом')
+const ork = new Ork('orks', 'ork', 'ru', 'Мечом');
 const elf = new Elf('elfs', 'elf', 'en', 'Лечебное зелье');
-elf.createIncantations()
-ork.break()
+
+elf.createIncantations(); 
+ork.break(); 
